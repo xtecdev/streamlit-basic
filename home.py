8bd3570655c04e5406c26a69aaef2d368253a79e
David de Mingo <david@optersoft.com>
2025-08-28 13:57:29 +0200

    Add st config

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ st.header("Uber pickups in NYC")
 
 data_load_state = st.text('Loading data...')
 data = db.load(10000)
-data_load_state.text('Loading data... Done! (using st.cache_data)')
+data_load_state.text('Loading data... Done!')
 
 if st.checkbox("Show raw data"):
     st.subheader("Raw data")
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+st.set_page_config(
+   page_title="Uber | xtec.dev",
+   page_icon="X",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+
 home = st.Page("home.py", title="Home", icon="🏠")
 histogram = st.Page("histogram.py", title="Histogram", icon="📊")
 map = st.Page("map.py", title="Map", icon="🗺️")
